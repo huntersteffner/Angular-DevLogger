85a4ce55eeadb11069c3bf9397de6b0eca812323
Matthew Hunter Steffner <huntersteffner@gmail.com>
2023-04-12 11:02:43 -0400

    Created dummy data

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LogService } from 'src/app/services/log.service';
+import { Log } from 'src/app/models/Log';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+  styleUrls: ['./logs.component.css'],
 })
-export class LogsComponent {
+export class LogsComponent implements OnInit {
+  logs!: Log[];
 
+
+  constructor(private logService: LogService) {
+    
+  }
+
+  ngOnInit() {
+    this.logs = this.logService.getLogs()
+  }
 }
